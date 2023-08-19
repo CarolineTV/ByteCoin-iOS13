5e2c3663a1d00df3c117eb8cae3e2a1b2a636aba
Caroline Tikhomirova <sonyukcv@gmail.com>
2023-08-19 17:29:28 +0400

    complete app

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Caroline Tikhomirova on 19.08.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Codable {
+    let rate: Double
+}
